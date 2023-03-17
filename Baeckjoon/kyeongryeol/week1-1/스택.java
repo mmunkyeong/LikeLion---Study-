2d03fa65892f519256d963034f9dd612e284ef41
KyeongRyeolKR <119229381+KyeongRyeolKR@users.noreply.github.com>
2023-03-17 18:33:34 +0900

    [Baekjoon-10828] 스택

---
+++ Baeckjoon/kyeongryeol/week1-1/스택.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            String[] input = br.readLine().split(" ");
+
+            switch (input[0]) {
+                case "push":
+                    stack.push(Integer.valueOf(input[1]));
+                    break;
+                case "pop":
+                    System.out.println(stack.isEmpty() ? -1 : stack.pop());
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    System.out.println(stack.isEmpty() ? 1 : 0);
+                    break;
+                case "top":
+                    System.out.println(stack.isEmpty() ? -1 : stack.peek());
+                    break;
+            }
+        }
+    }
+}
