7dfd1ef2f19979c2712b889d8fd0f7b589251a71
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-03-23 14:44:19 +0900

    [Programmers-12918] 문자열 다루기 기본

---
+++ Programmers/seungchan/week2-1/문자열 다루기 기본.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        for (char x : s.toCharArray()) {
+            if (!Character.isDigit(x)) {
+                return false;
+            }
+        }
+        return answer;
+    }
+}
