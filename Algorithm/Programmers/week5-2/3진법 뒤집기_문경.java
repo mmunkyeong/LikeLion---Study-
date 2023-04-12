aa117c4cc6ec7e6d0ecfdfa8cede626c573f9564
문경 <62290451+mmunkyeong@users.noreply.github.com>
2023-04-12 23:49:13 +0900

    [Programmers-68935] 3진법 뒤집기

--- Algorithm/Programmers/week5-2/3진법 뒤집기_문경.java
+++ Algorithm/Programmers/week5-2/3진법 뒤집기_문경.java
@@ -8,15 +8,15 @@ class Solution {
             if(n<=0){
                 break;
             }
-            num=n%3;
-            numStr+=String.valueOf(num);
+            num=n%3; 
+            numStr+=String.valueOf(num);  //3으로 나눈 나머지를 numStr에 더하기 
             n/=3;
         }
         
-        int idx=1;
+        int idx=1; // 제곱수를 나타내는 변수 1, 3, 9, 27..
         for(int i=numStr.length()-1; i>=0; i--){
             answer+= Character.getNumericValue(numStr.charAt(i))*idx;
-            idx*=3;
+            idx*=3; 
         }
         return answer;
     }
